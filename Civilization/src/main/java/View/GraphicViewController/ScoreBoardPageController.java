43da0f51fb1556479d8168740214fbdc71be32a7
Arash <arashziyaei2013@yahoo.com>
2022-07-17 09:30:02 +0430

    score board changed

--- Civilization/src/main/java/View/GraphicViewController/ScoreBoardPageController.java
+++ Civilization/src/main/java/View/GraphicViewController/ScoreBoardPageController.java
@@ -10,8 +10,14 @@ import javafx.animation.Timeline;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.util.Duration;
@@ -30,13 +36,34 @@ public class ScoreBoardPageController implements Initializable {
         text1.setStyle("-fx-font-family: 'Cooper Black' ; -fx-fill: #86033b; -fx-font-size: 60;");
         list.getChildren().add(text1);
         for (User user: LoginAndRegisterController.getInstance().getUsers()) {
-            Text text = new Text(i+"_ " + user.getUsername() + "    __   Score: " + user.getScore());
-            list.getChildren().add(text);
-            text.setStyle("-fx-font-family: 'Cooper Black' ; -fx-fill: #4b0862; -fx-font-size: 50;");
-            if(i<4)text.setStyle("-fx-font-family: 'Cooper Black' ; -fx-fill: #8a7939; -fx-font-size: 50;");
+            Label text = new Label("\n"+user.getUsername() + "\t\t\t\t\t\t\t" + user.getScore());
+            text.setStyle("-fx-font-size: 20; -fx-font-family: Garamond;");
+            ImageView imageView = null;
+            if(i == 1){
+                imageView = creatingImageView("/images/scoreBoard/firstPlace.png", 80, 80);
+            } else if (i == 2){
+                imageView = creatingImageView("/images/scoreBoard/secondPlace.png", 80, 80);
+            } else if (i == 3){
+                imageView = creatingImageView("/images/scoreBoard/thirdPlace.png", 80, 80);
+            } else {
+                imageView = creatingImageView("/images/scoreBoard/"+i+".png", 80, 80);
+            }
+            String profIndex = String.valueOf(user.getProfPicIndex() + 1);
+            ImageView u_imageView = creatingImageView("/images/profiles/"+profIndex+".png", 60, 60);
+            HBox u_hBox = new HBox(imageView, u_imageView, new Separator(), text);
+            text.setAlignment(Pos.CENTER_RIGHT);
+            list.getChildren().add(u_hBox);
+            list.getChildren().add(new Separator());
             i++;
-            if(i == 11)break;
+            if(i == 11) break;
         }
+    }
+    private javafx.scene.image.ImageView creatingImageView(String address, int FitWidth, int FitHeight){
+        Image image = new Image(address);
+        javafx.scene.image.ImageView imageView = new javafx.scene.image.ImageView(image);
+        if(FitWidth != 0) imageView.setFitWidth(FitWidth);
+        if(FitHeight != 0) imageView.setFitHeight(FitHeight);
+        return imageView;
     }
 
     public void buttonSizeIncrease(MouseEvent mouseEvent) {
